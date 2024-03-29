98b248d80300e446bfce47c33bfb3b65b506fea6
sbx_user1051 <sbx_user1051@169.254.74.61>
2024-03-29 17:46:01 +0000

    Fri Mar 29 17:46:01 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,42 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        int total = 0;
+        Map<String, Integer> itens = new HashMap<>();
+        for (int i = 0; i < skus.length(); i++) {
+            String iten = String.valueOf(i);
+            if (itens.get(iten) != null) {
+                int quant = itens.get(iten);
+                quant++;
+                itens.put(iten, quant);
+            } else {
+                itens.put(iten, 1);
+            }
+        }
+        for (String sku : itens.keySet()) {
+            int quantSpecialOffers;
+            int quantNormalValue;
+            switch (sku) {
+                case "A" -> {
+                    quantSpecialOffers = itens.get(sku) / 3;
+                    quantNormalValue = itens.get(sku) % 3;
+                    total += quantSpecialOffers * 130 + quantNormalValue * 50;
+                }
+                case "B" -> {
+                    quantSpecialOffers = itens.get(sku) / 2;
+                    quantNormalValue = itens.get(sku) % 2;
+                    total += quantSpecialOffers * 45 + quantNormalValue * 30;
+                }
+                case "C" -> total += itens.get(sku) * 20;
+                case "D" -> total += itens.get(sku) * 15;
+            }
+        }
+        return total;
     }
 }
+
